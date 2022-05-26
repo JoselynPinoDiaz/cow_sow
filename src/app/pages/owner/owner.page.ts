4c3af9147cc019c302b32809da614fce5f073cb6
Your Name <you@example.com>
2022-05-26 12:03:38 -0400

    .

--- src/app/pages/owner/owner.page.ts
+++ src/app/pages/owner/owner.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
+
+
 
 /* Importacion de componentes a utilizar */
 
@@ -8,10 +12,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./owner.page.scss'],
 })
 export class OwnerPage implements OnInit {
+  navCtrl: any;
 
-  constructor() { }
+
+  constructor( private menu: MenuController) { }
 
   ngOnInit() {
   }
 
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+
+  
+
 }
+
